5783e5816915cc6fdbb010d8d81ec521bdb4dc08
professorkazarinoff <peter.kazarinoff@pcc.edu>
2017-11-27 22:35:03 -0800

    another publish try

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://professorkazarinoff.github.io/staticsite/'
-RELATIVE_URLS = False
+SITEURL = 'https://professorkazarinoff.github.io/staticsite'
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'

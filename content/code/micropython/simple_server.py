c313eeca692fdd3160f10c51b36b26013cd69bd5
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-02-26 22:14:44 -0800

    published changes

---
+++ content/code/micropython/simple_server.py
@@ -0,0 +1,35 @@
+
+def simpleserver():
+    import machine
+    pins = [2]
+
+    html = """<!DOCTYPE html>
+    <html>
+        <head> <title>ESP8266 Pins</title> </head>
+        <body> <h1>ESP8266 Pins</h1>
+            <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+        </body>
+    </html>
+    """
+
+    import socket
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    print('listening on', addr)
+
+    while True:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+        response = html % '\n'.join(rows)
+        cl.send(response)
+        cl.close()
--- content/posts/plotting_sin_cos_with_matplotlib.md
+++ content/posts/plotting_sin_cos_with_matplotlib.md
@@ -1,4 +1,4 @@
-Title: Plotting sin and cos with matplotlib and Python
+Title: Plotting sine and cosine with matplotlib and Python
 Slug: plotting-sin-cos-with-matplotlib
 Date: 2018-02-05 10:30
 Status: published
@@ -6,6 +6,6 @@ Category: Plotting
 Tags: python, jupyter, matplotlib, numpy, plots
 JavaScripts: ipynb_mathjax_inline.js
 Author: Peter D. Kazarinoff
-Summary: In this post we will create a plot using matplotlib and Python. The plot will show two trig functions, sin and cos on the same set of axes.
+Summary: In this post we will create a plot using matplotlib and Python. The plot will show two trig functions, sine and cosine on the same set of axes.
 
 {% notebook ../code/matplotlib_plots/plotting_trig_functions.ipynb %}

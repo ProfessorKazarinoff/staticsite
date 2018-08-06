7e3131819f5d4e71524683ecd30d9749c681591f
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-08-06 13:14:51 -0700

    adds

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://pythonforundergradengineers.com'
+SITEURL = 'https://pythonforundergradengineers.com'
 RELATIVE_URLS = True
 
 # RSS Feed Settings

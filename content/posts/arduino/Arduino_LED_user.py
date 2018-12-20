2adddaf482995421058f4e77914d547f0197b540
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-12-19 18:15:22 -0800

    ran black on posts/arduino .py files

--- content/posts/arduino/Arduino_LED_user.py
+++ content/posts/arduino/Arduino_LED_user.py
@@ -5,29 +5,31 @@ import time
 
 # Define the serial port and baud rate.
 # Ensure the 'COM#' corresponds to what is shown in the Windows Device Manager
-ser = serial.Serial('COM15', 9600)
+ser = serial.Serial("COM15", 9600)
+
 
 def led_on_off():
     user_input = input("\n Type on / off / quit : ")
-    if user_input =="on":
+    if user_input == "on":
         print("The LED is on...")
-        time.sleep(0.1) 
-        ser.write(b'H') 
+        time.sleep(0.1)
+        ser.write(b"H")
         led_on_off()
-    elif user_input =="off":
+    elif user_input == "off":
         print("The LED is off...")
         time.sleep(0.1)
-        ser.write(b'L')
+        ser.write(b"L")
         led_on_off()
-    elif user_input =="quit" or user_input == "q":
+    elif user_input == "quit" or user_input == "q":
         print("Program Exiting")
         time.sleep(1)
-        ser.write(b'L')
+        ser.write(b"L")
         ser.close()
     else:
         print("Invalid input. Type on / off / quit.")
         led_on_off()
 
-time.sleep(2) # wait for the serial connection to initialize
+
+time.sleep(2)  # wait for the serial connection to initialize
 
 led_on_off()
--- content/posts/arduino/Arduino_blink.py
+++ content/posts/arduino/Arduino_blink.py
@@ -4,8 +4,8 @@ import serial
 import time
 
 for i in range(10):
-    with serial.Serial('COM15', 9800, timeout=1) as ser:
+    with serial.Serial("COM15", 9800, timeout=1) as ser:
         time.sleep(2)
-        ser.write(b'H')   # send a byte
-        time.sleep(0.5)   # wait 0.5 seconds
-        ser.write(b'L')   # send a byte
+        ser.write(b"H")  # send a byte
+        time.sleep(0.5)  # wait 0.5 seconds
+        ser.write(b"L")  # send a byte

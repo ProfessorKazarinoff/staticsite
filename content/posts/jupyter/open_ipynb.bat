c1ca6cc2e6ccfdbc357e2b07f1a0d75e11ad2e43
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2022-12-16 14:05:34 -0800

    add open_ipynb.bat file for double click ipynb post

---
+++ content/posts/jupyter/open_ipynb.bat
@@ -0,0 +1,3 @@
+@echo off
+set ANACONDAPATH=C:\Users\Peter\Anaconda3
+%ANACONDAPATH%\python.exe %ANACONDAPATH%\cwp.py %ANACONDAPATH%^ %ANACONDAPATH%\python.exe %ANACONDAPATH%\Scripts\jupyter-notebook-script.py %1
